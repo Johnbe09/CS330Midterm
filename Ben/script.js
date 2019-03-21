17d86e4c113e9936899b7b80a36d1df5124d1d37
johnbe09 <johnbe09@github.com>
2019-03-20 20:53:49 -0500

    working weather and national parks

--- Ben/script.js
+++ Ben/script.js
@@ -13,8 +13,8 @@ async function getData(url) {
 }
 
 async function topGame() {
-    let numb = document.getElementById('input_date').value;
-    console.log(numb);
+    let park = document.getElementById('input_date').value;
+    console.log(park);
     // let [scheduleData, teamData, standingsData] = await Promise.all([
     //     getData('http://data.nba.net/json/v1/2018/schedule.json'),
     //     getData('http://data.nba.net/json/v2/2018/teams.json'),
@@ -22,31 +22,41 @@ async function topGame() {
     // ]);
     // console.log(standingsData);
     
-    let city = 'Decorah';
+    // let park = 'acad';
     let [parkData] = await Promise.all([
         // getData('https://stats.nba.com/stats/scoreboard/?GameDate=03/18/2019&LeagueID=00&DayOffset=0')
         // getData("https://stats.nba.com/js/data/playermovement/NBA_Player_Movement.json")
         // getData("http://data.nba.net/json/cms/2018/league/nba_games.json")
-        getData(`https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=N9qYwDPpR2NIBkGg1YPr6FfGVsjc0xhFfON7ZmPN`)
+        getData(`https://developer.nps.gov/api/v1/parks?parkCode=${park}&api_key=N9qYwDPpR2NIBkGg1YPr6FfGVsjc0xhFfON7ZmPN`)
     ]);
     console.log(parkData);
 
-    let lat = parkData.data;
-    console.log(lat);
-    let lon = '-114';
+    let latlong = parkData.data[0].latLong;
+    console.log(latlong);
+    let latlongArray = latlong.split(" ");
+    let lat = latlongArray[0].split(":");
+    lat = lat[1].split(",");
+    lat = lat[0];
+    let long = latlongArray[1].split(":");
+    long = long[1];
 
-    // let [cityWeather] = await Promise.all([
+    console.log(lat);
+    console.log(long);
+
+    // let lon = '-114';
+
+    let [cityWeather] = await Promise.all([
     //     // getData('https://stats.nba.com/stats/scoreboard/?GameDate=03/18/2019&LeagueID=00&DayOffset=0')
     //     // getData("https://stats.nba.com/js/data/playermovement/NBA_Player_Movement.json")
     //     // getData("http://data.nba.net/json/cms/2018/league/nba_games.json")
     //     //getData(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=2b631a9cde7c2b110c438c3565bf8c5d`)
-    //     getData(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=2b631a9cde7c2b110c438c3565bf8c5d`)
-    // ]);
-    // let temperatureK = cityWeather.main.temp;
-    // let temperatureF = (temperatureK -273.15) * 1.8 +32;
-    // temperatureF = temperatureF.toFixed(2);
-    // // console.log(cityWeather);
-    // console.log(temperatureF);
+        getData(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=2b631a9cde7c2b110c438c3565bf8c5d`)
+    ]);
+    let temperatureK = cityWeather.main.temp;
+    let temperatureF = (temperatureK -273.15) * 1.8 +32;
+    temperatureF = temperatureF.toFixed(2);
+    // console.log(cityWeather);
+    console.log(temperatureF);
 
 
 
